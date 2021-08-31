cc46373d699be6a8f8e1e9da5f4a16e44387b243
oussamaghrib <oussghrib@gmail.com>
2021-08-31 22:56:52 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,16 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
 
+import DirectionProvider, { DIRECTIONS } from 'react-with-direction/dist/DirectionProvider';
+
+
 class IndexPage extends React.Component {
   render() {
     const siteTitle = "Gatsby Starter Personal Website"
 
     return (
+      <DirectionProvider direction={DIRECTIONS.RTL}>
+        <div>
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="Home"
@@ -17,12 +22,12 @@ class IndexPage extends React.Component {
         />
         <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
-          Hey people{" "}
+          مرحبا بالحبايب{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>هته هيس الطريقة الرائعى بسيكبنسي</p>
         <p>
           This starter comes out of the box with styled components and Gatsby's
           default starter blog running on Netlify CMS.
@@ -32,6 +37,8 @@ class IndexPage extends React.Component {
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
       </Layout>
+      </div>
+      </DirectionProvider>
     )
   }
 }
